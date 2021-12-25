0c82fff4157d70488992d2516898ed1cbd28096d
Oscar Beaumont <oscar@otbeaumont.me>
2021-12-25 14:58:40 +0800

    fix: fixed error causing invalid GraphQL schema

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -103,7 +103,7 @@ pub fn derive_relay_interface(input: TokenStream) -> TokenStream {
 
         #(#impls)*
 
-        #[async_graphql::Scalar]
+        #[async_graphql::Scalar(name = "RelayNodeID")]
         impl async_graphql::ScalarType for #ident {
             fn parse(value: async_graphql::Value) -> async_graphql::InputValueResult<Self> {
                 unimplemented!();
